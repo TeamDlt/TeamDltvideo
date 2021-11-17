08d3f1b0fa1aa6345edea5d19e8587b1b5dc3cb0
TeamDlt <93529114+TeamDlt@users.noreply.github.com>
2021-11-17 17:35:56 +0530

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -2,6 +2,6 @@ import os
 from config import ASSISTANT_NAME
 
 HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stopstream`  **: Stop the stream**"
-START_TEXT = '👋 `Hey My Name Is` **VcVideoPlayer** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
+START_TEXT = '👋 `Hey My Name Is` **TEAMDLTVIDEO** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
 ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group video chats using several methods from WebRTC.` \n\n`💡 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
 DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
